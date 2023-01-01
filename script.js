4fb3bffb94ddf26e86e080b155f45fff3831ec34
AM_00 <taole.chen@gmail.com>
2023-01-01 20:03:09 +0100

    sketching function implemented.

--- script.js
+++ script.js
@@ -8,6 +8,20 @@ class View {
         for (let i = 0; i < 256; i++) {
             let cell = document.createElement('div');
             cell.classList.add('cell');
+            cell.setAttribute('id', 'c' + i.toString());
+            cell.style.backgroundColor = 'rgb(255,255,255)';
+            cell.addEventListener('mouseenter', function (e) {
+                var c = document.querySelector("#" + e.target.id);
+                //console.log(c.style.backgroundColor);
+
+                var rgb = View.colorValues(c.style.backgroundColor);
+                for (let i = 0; i < 3;i++) {
+                    if (rgb[i] >= 20) rgb[i] = rgb[i] - 20;
+                }
+                var newcol = 'rgb(' + rgb[0] + ',' + rgb[1] + ',' + rgb[2] + ')';
+                console.log(newcol);
+                c.style.backgroundColor = newcol;
+            });
             this.cells[i] = cell;
             this.gridContainer.appendChild(cell);
         }
@@ -28,6 +42,55 @@ class View {
         }
 
         
+    }
+
+    onHover(cell) {
+        let c = this.style.backgroundColor;
+        console.log(cell);
+
+    }
+
+    // return array of [r,g,b,a] from any valid color. if failed returns undefined
+    static colorValues(color){
+        if (color === '')
+            return;
+        if (color.toLowerCase() === 'transparent')
+            return [0, 0, 0, 0];
+        if (color[0] === '#')
+        {
+            if (color.length < 7)
+            {
+                // convert #RGB and #RGBA to #RRGGBB and #RRGGBBAA
+                color = '#' + color[1] + color[1] + color[2] + color[2] + color[3] + color[3] + (color.length > 4 ? color[4] + color[4] : '');
+            }
+            return [parseInt(color.substr(1, 2), 16),
+                parseInt(color.substr(3, 2), 16),
+                parseInt(color.substr(5, 2), 16),
+                color.length > 7 ? parseInt(color.substr(7, 2), 16)/255 : 1];
+        }
+        if (color.indexOf('rgb') === -1)
+        {
+            // convert named colors
+            var temp_elem = document.body.appendChild(document.createElement('fictum')); // intentionally use unknown tag to lower chances of css rule override with !important
+            var flag = 'rgb(1, 2, 3)'; // this flag tested on chrome 59, ff 53, ie9, ie10, ie11, edge 14
+            temp_elem.style.color = flag;
+            if (temp_elem.style.color !== flag)
+                return; // color set failed - some monstrous css rule is probably taking over the color of our object
+            temp_elem.style.color = color;
+            if (temp_elem.style.color === flag || temp_elem.style.color === '')
+                return; // color parse failed
+            color = getComputedStyle(temp_elem).color;
+            document.body.removeChild(temp_elem);
+        }
+        if (color.indexOf('rgb') === 0)
+        {
+            if (color.indexOf('rgba') === -1)
+                color += ',1'; // convert 'rgb(R,G,B)' to 'rgb(R,G,B)A' which looks awful but will pass the regxep below
+            return color.match(/[\.\d]+/g).map(function (a)
+            {
+                return +a
+            });
+        }
     }
 }
 
